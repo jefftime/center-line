4d2c59ebf3407dd470a7cfa8ff99f5135f0ce2b6
Jeffery Stager <jffry09@gmail.com>
2019-09-15 13:57:53 -0400

    add copyright

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 /* This file is part of CenterLine
  *
+ * Copyright 2019, Jeffery Stager
+ *
  * CenterLine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
